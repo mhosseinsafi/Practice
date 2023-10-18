e61e21cebee930ae58deee0a0541bf2c971526d5
Hossein <hossein.saafi@gmail.com>
2023-10-17 18:15:41 -0700

    add route and switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./style/App.css";
-import React from "react";
-import Home from "./components/home.js"
-// import data from "./data.json";
-// import Quiz from "./asset/THELOGO.png";
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import home from './home';
+import instruction from './instruction';
 
 function App() {
   return (
-    <Home />
-  )
+    <Router>
+      <Switch>
+        <Route exact path="/" component={home} />
+        <Route path="/instructions" component={instruction} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
+
